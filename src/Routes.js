308c7d70b4634cedbe0c2499098bdee21fd2af9b
Bhabesh Pradhan <bhabeshchandrapradhan@gcekbpatna.ac.in>
2023-06-24 12:11:19 +0530

    Setting up our React app

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+// import Login from "./containers/Login";
+// import Signup from "./containers/Signup";
+// import NewNote from "./containers/NewNote"; 
+
+export default function MyRoutes() {
+  return (
+    <Routes>
+      {/* <Route path="/Login" element={<Login />} /> */}
+      <Route path="/" element={<Home />} />
+      {/* Finally, catch all unmatched routes */}
+      <Route path="*" element={<NotFound />} />
+      {/* <Route path="/signup" element={<Signup />} /> */}
+      {/* <Route path="/notes/new" element={<NewNote />} />  */}
+    </Routes>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "bhabesh-bucket",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://cwuf676t90.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_iTQ8X3M0d",
+    APP_CLIENT_ID: "3jkmonve56q3lk2tvj5ia18fdl",
+    IDENTITY_POOL_ID: "us-east-1:2a1a14d4-5231-4c61-acd8-fca28dae0ae1",
+  },
+};
+export default config;
